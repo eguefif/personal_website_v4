53833c9f5a925ec2d5c2b28d5105cb3c4dd18cf1
Emmanuel Guefif <eguefif@fastmail.com>
2025-07-15 19:59:44 -0400

    Add main layout
    A top header with the general menu: professional page and blog
    
    The blog does not work for now.
    The professional page is divided into a side menu and a main content

---
+++ frontend/src/components/ProfessionalPage/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components';
+
+export default function Header() {
+  return (
+    <Nav aria-label="Website main menu">
+      <NavLink href="/sale">Professional page</NavLink>
+      <NavLink href="/sale">Blog</NavLink>
+    </Nav>
+  );
+}
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 34px;
+
+  height: 64px;
+  width: 450px;
+  margin: auto;
+  padding: 34px
+`;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  flex: 1;
+  text-align: center;
+  color: var(--color-primary);
+`;
---
+++ frontend/src/components/ProfessionalPage/ProfessionalContent/ProfessionalContent.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export default function ProfessionalContent() {
+  return (
+    <Wrapper>
+      <div>Main Content</div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+
+`;
