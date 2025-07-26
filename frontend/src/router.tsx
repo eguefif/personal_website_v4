75a955c0e39a9c74c577b3c8b8012dbca8b0ee71
Emmanuel Guefif <eguefif@fastmail.com>
2025-07-25 22:06:45 -0400

    refactor router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,50 +2,17 @@ import { createBrowserRouter, Navigate } from 'react-router';
 
 import App from './App.tsx'
 
-import Admin from 'components/Admin/Admin';
-
-import AdminProfessionalPage from 'components/Admin/Professional/AdminProfessionalPage';
-import EditExperience from 'components/Admin/Professional/EditExperience';
-import EditPersonal from 'components/Admin/Professional/EditPersonal';
-import EditPortfolio from 'components/Admin/Professional/EditPortfolio';
 
 import BlogPage from 'components/Admin/BlogPage';
 
+import adminRouter from 'components/Admin/AdminRouter';
 
 const router = createBrowserRouter([
   {
     path: '/',
     Component: App,
   },
-  {
-    path: '/admin',
-    Component: Admin,
-    children: [
-      { 
-        index: true, 
-        element: <Navigate to="professional" replace /> 
-      },
-      { 
-        path: 'professional',
-        Component: AdminProfessionalPage,
-        children: [
-          {
-            path: 'personal',
-            Component: EditPersonal
-          },
-          {
-            path: 'experience',
-            Component: EditExperience
-          },
-          {
-            path: 'portfolio',
-            Component: EditPortfolio
-          },
-        ],
-      },
-      { path: 'blog', Component: BlogPage },
-    ]
-  }
+  adminRouter,
 ]);
 
 export default router;
