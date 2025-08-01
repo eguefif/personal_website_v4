d771a760d184d640b2f33fe1d4fddde86d949ca0
Emmanuel Guefif <eguefif@fastmail.com>
2025-07-31 20:25:45 -0400

    feat: add tech tags to project and experience cards
    Introduce TechList and TechEntry components to display technology tags
    in ProjectCard and ExperienceCard. Refactor ProjectCard layout to include
    a dedicated tech section below project details for better clarity and
    visual separation. Adjust styling for consistent spacing and rounded
    corners. This improves the presentation of technologies used in projects
    and experiences, enhancing user understanding of skills involved.

--- frontend/src/components/ProfessionalPage/ProfessionalContent/PortFolio/ProjectCard.tsx
+++ frontend/src/components/ProfessionalPage/ProfessionalContent/PortFolio/ProjectCard.tsx
@@ -3,40 +3,60 @@ import { styled } from 'styled-components';
 import type { Project } from './FakeData';
 import { H2 } from 'components/Commons/Titles';
 
+import { TechList, TechEntry } from 'components/Commons/TechTags';
+
 export default function ProjectCard({project} : { project: Project }) {
+
+  const techEntries = project.tech.map((tech, index) =>
+    <TechEntry key={index}>{tech}</TechEntry>
+  );
   return (
     <ProjectCardWrapper>
-      <ImageWrapper>
-        <ProjectImage src={project.image.src} alt={project.image.alt} />
-      </ImageWrapper>
-      <ProjectContent>
-        <ProjectTitle>
-          {project.title}
-        </ProjectTitle>
-        <ProjectDescription>
-          {project.description}
-        </ProjectDescription>
-      </ProjectContent>
+
+      <TopWrapper>
+        <ImageWrapper>
+          <ProjectImage src={project.image.src} alt={project.image.alt} />
+        </ImageWrapper>
+        <ProjectContent>
+          <ProjectTitle>
+            {project.title}
+          </ProjectTitle>
+          <ProjectDescription>
+            {project.description}
+          </ProjectDescription>
+        </ProjectContent>
+      </TopWrapper>
+
+      <TechWrapper>
+        <TechList>
+          {techEntries}
+        </TechList>
+      </TechWrapper>
     </ProjectCardWrapper>
   );
 }
 
 const ProjectCardWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   background-color: var(--color-gray-400-alpha-20);
   border-radius: 16px;
   gap: 16px;
 `;
 
+const TopWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
 const ImageWrapper = styled.div`
   max-width: 224px;
-  max-height: 224px;
 `;
 
 const ProjectImage = styled.img`
-  border-radius: 16px 0px 0px 16px;
+  border-radius: 16px 0px 0px 0px;
   width: 100%;
-  height: 100%;
+  aspect-ratio: 1/1;
   object-fit: cover;
 `;
 
@@ -52,4 +72,8 @@ const ProjectTitle = styled(H2)`
 `;
 
 const ProjectDescription = styled.div`
+`;
+
+const TechWrapper = styled.div`
+  padding: 12px;
 `;
